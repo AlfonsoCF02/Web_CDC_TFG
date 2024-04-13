6b0b37b3cf12e2024e2dc97629a6ac1b68880036
Alfonso Cabezas Fernández <alfonso.acf66@gmail.com>
2024-04-13 21:47:35 +0200

    Fix - Production deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 100,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
   plugins: [react()],
-})
+});
